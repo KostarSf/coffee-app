2f5dcfe345adb214595e30ce683f504c1c915169
Maksim Peskov <kostarsf@gmail.com>
2024-03-05 12:20:01 +0300

    feat: add Button component and design system tokens

---
+++ shared/Button/Button.tsx
@@ -0,0 +1,29 @@
+import { Pressable, PressableProps, StyleSheet, Text, View } from 'react-native';
+import { Colors, Fonts, Radius } from './tokens';
+
+type ButtonProps = PressableProps & { title: string };
+export function Button({ title, ...props }: ButtonProps) {
+    return (
+        <Pressable {...props}>
+            <View style={styles.button}>
+                <Text style={styles.text}>{title}</Text>
+            </View>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        height: 62,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primary,
+        borderRadius: Radius.r16,
+    },
+
+    text: {
+        color: Colors.white,
+        fontSize: Fonts.f16,
+        fontWeight: '600',
+    },
+});
---
+++ shared/Button/tokens.ts
@@ -0,0 +1,26 @@
+export const Colors = {
+    black: 'black',
+    gray: '#A9A9A9',
+    white: 'white',
+    primary: '#C67C4E',
+    primaryHover: '#A76237',
+};
+
+export const Gaps = {
+    g8: 8,
+    g24: 24,
+};
+
+export const Fonts = {
+    f14: 14,
+    f16: 16,
+    f34: 34,
+}
+
+export const Radius = {
+    r16: 16,
+}
+
+export const Letter = {
+    l1: 1,
+}
